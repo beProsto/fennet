f6c626212478008f2b9630bc04a16b246025dec1
beProsto <lavionperavion@gmail.com>
2022-12-23 00:36:57 +0100

    started work on working with doxygen documentation

--- examples/HelloServer/main.c
+++ examples/HelloServer/main.c
@@ -1,5 +1,19 @@
+/**
+ * @file main.c
+ * @brief HelloServer example source code.
+ *
+ * HelloServer is a simple example of fennet.
+ * It serves an HTTP server that always responds with the text "Hello!".
+ */
+
 #include <fennet/lib.h>
 #include <stdio.h>
+
+/**
+ * @brief Main entry point of the program.
+ *
+ * A longer description.
+ */
 
 int
 main(int argc, char** argv)
--- include/fennet/lib.h
+++ include/fennet/lib.h
@@ -5,5 +5,12 @@
 #define NAH 0
 #define YUP 1
 
+/**
+ * @brief Main entry point of the program.
+ *
+ * A longer description.
+ *
+ * @return @c a 0 is to be returned.
+ */
 int FEN_API
 doesItLookLikeImBoring();
