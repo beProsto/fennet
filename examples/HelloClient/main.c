c6bb41800ef376d71368c85ad806e32de1b2648e
beProsto <lavionperavion@gmail.com>
2022-12-28 13:30:00 +0100

    [make] export clangd compile commands upon building

--- examples/HelloClient/main.c
+++ examples/HelloClient/main.c
@@ -24,54 +24,7 @@
 int
 main(int argc, char** argv)
 {
-  int sockfd, connfd, len;
-  struct sockaddr_in servaddr = {}, cli;
-
-  // socket create and verification
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd == -1) {
-    printf("socket creation failed...\n");
-    exit(0);
-  } else
-    printf("Socket successfully created..\n");
-
-  // assign IP, PORT
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port = htons(PORT);
-
-  // Binding newly created socket to given IP and verification
-  if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
-    printf("socket bind failed...\n");
-    exit(0);
-  } else
-    printf("Socket successfully binded..\n");
-
-  // Now server is ready to listen and verification
-  if ((listen(sockfd, 5)) != 0) {
-    printf("Listen failed...\n");
-    exit(0);
-  } else
-    printf("Server listening..\n");
-  len = sizeof(cli);
-
-  // Accept the data packet from client and verification
-  connfd = accept(sockfd, (SA*)&cli, &len);
-  if (connfd < 0) {
-    printf("server accept failed...\n");
-    exit(0);
-  } else
-    printf("server accept the client...\n");
-
-  char buffer[1024] = {};
-
-  read(connfd, buffer, 1024);
-  printf("From client: %s\n", buffer);
-
-  // intentionally leak memory (do a little trolling)
-  write(connfd, "HTTP/1.1 200 OK\r\n\r\nAAAAAAAAHELPME", 1024);
-
-  close(sockfd);
+  doesItLookLikeImBoring();
 }
 
 /** Wiem że między nami już jakiś czas nie jest najlepiej, i, jak już ci pisałem
